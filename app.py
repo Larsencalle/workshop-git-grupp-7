3fe54afe28b6efa8988e004b19d87e6912b3c80b
isatousanekvist-ops <isatou.sane.kvist@gmail.com>
2026-03-25 15:58:27 +0100

    Docs: add run () docstring

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from calculator import add, sub, mul, div
 #simple console calculator
 
 def run():
+    """Console calculator entry post"""
     print("1) Add\n2) Subtract\n3) Multiply\n4) Divide\n0) Exit")
     choice = input("Select: ")
 
