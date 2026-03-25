e35015eb78f5d053cbba8d8fc98a98baa3812438
Tildap16 <tildispettersson@me.com>
2026-03-25 15:40:24 +0100

    Import div() and handle choice 4

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from calculator import add, sub
+from calculator import add, sub, mul, div
 def run():
     print("1) Add\n2) Subtract\n3) Multiply\n4) Divide\n0) Exit")
     choice = input("Select: ")
@@ -6,3 +6,4 @@ def run():
     if choice == "2": print(("sub(a, b)"))
     a = float(input("a: "))
     b = float(input("b: "))
+    if choice == "4": print(div(a, b))
