a75deebf510d674a21a9834beef96c8c397592d6
fredrik-spec <fredrik@hamiltonhouse.se>
2026-03-25 15:38:56 +0100

    Import mul() and handle choice 3

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-from calculator import add
+from calculator import add, sub, mul
 def run():
     print("1) Add\n2) Subtract\n3) Multiply\n4) Divide\n0) Exit")
     choice = input("Select: ")
 
+    if choice == "3": print(mul(a,b))
     if choice == "1": print("add(a, b)")
     a = float(input("a: "))
     b = float(input("b: "))
